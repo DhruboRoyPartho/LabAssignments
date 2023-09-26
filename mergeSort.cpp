3b579f035d1f16dbb11ff609e674b38963d1056e
Dhrubo Roy Partho <96194220+DhruboRoyPartho@users.noreply.github.com>
2023-09-26 22:28:28 +0600

    mergeSort.cpp

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,33 +3,34 @@ using namespace std;
 
 void merge(int arr[], int l, int mid, int r)
 {
-    int n1 = mid-l+1;
-    int n2 = r-mid;
-
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
     int a[n1], b[n2];
     for(int i=0;i<n1;i++){
-        a[i] = arr[i+l];
+        a[i] = arr[l+i];
+    }
+    for(int i=0;i<n2;i++){
         b[i] = arr[mid+i+1];
     }
 
     int i=0,j=0,k=l;
-    while(i<n1 && j <n2){
+    while(i < n1 && j < n2){
         if(a[i] < b[j]){
             arr[k] = a[i];
-            k++,i++;
+            k++, i++;
         }
         else{
             arr[k] = b[j];
-            k++,j++;
+            k++, j++;
         }
     }
     while(i<n1){
         arr[k] = a[i];
-        k++,i++;
+        i++, k++;
     }
     while(j<n2){
         arr[k] = b[j];
-        k++,j++;
+        j++, k++;
     }
 }
 
@@ -39,18 +40,18 @@ void mergeSort(int arr[], int l, int r)
         int mid = (l+r)/2;
         mergeSort(arr, l, mid);
         mergeSort(arr, mid+1, r);
-
         merge(arr, l, mid, r);
     }
 }
 
 int main()
 {
-    int arr[] = {0, 3, 2, 9, 10, 5, 12, 15};
+    int arr[] = {10, 20, 9, 0, 1, 7, 9, 3, 10};
     int len = sizeof(arr)/sizeof(arr[0]);
     mergeSort(arr, 0, len);
-    for(auto &q:arr){
-        cout<<q<<" ";
+    for(auto &a:arr){
+        cout<<a<<" ";
     }
+    cout<<endl;
     return 0;
 }
