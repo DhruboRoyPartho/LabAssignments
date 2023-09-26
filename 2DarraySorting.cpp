8375f12589ea23c3f77c8927d641d8a0aa719195
Dhrubo Roy Partho <96194220+DhruboRoyPartho@users.noreply.github.com>
2023-09-27 01:02:35 +0600

    Update 2DarraySorting.cpp

--- 2DarraySorting.cpp
+++ 2DarraySorting.cpp
@@ -4,21 +4,6 @@ using namespace std;
 #define ARR_SIZE 100
 int data[ARR_SIZE][ARR_SIZE], singleArray[ARR_SIZE*ARR_SIZE+1];
 
-//merge sort begin
-void bubble_sort(int data[], int len)
-{
-    for(int i=0;i<len;i++){
-        for(int j=0;j<len-i;j++){
-            if(data[j] > data[j+1]){
-                swap(data[j], data[j+1]);
-            }
-        }
-    }
-    return;
-}
-//Bubble sort end
-
-
 void takeValue(int n)
 {
     for(int i=0;i<n;i++){
