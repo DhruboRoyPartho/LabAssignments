1404111adfbad5ca31fa0fcd3a8a4067a4dad175
Dhrubo Roy Partho <96194220+DhruboRoyPartho@users.noreply.github.com>
2023-11-07 22:08:21 +0600

    Update SelectionBubbleInsertion_sort.cpp

--- Task-2/SelectionBubbleInsertion_sort.cpp
+++ Task-2/SelectionBubbleInsertion_sort.cpp
@@ -3,12 +3,24 @@ using namespace std;
 
 void selection_sort(int data[], int n)
 {
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(data[i] > data[j]){
-                swap(data[i], data[j]);
-            }
+    int i, j, min_idx;
+ 
+    // One by one move boundary of
+    // unsorted subarray
+    for (i = 0; i < n - 1; i++) {
+ 
+        // Find the minimum element in
+        // unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
         }
+ 
+        // Swap the found minimum element
+        // with the first element
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
     }
     return;
 }
